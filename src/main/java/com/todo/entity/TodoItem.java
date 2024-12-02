8b78f4b103d7cd5e244b8de0b1bdcdd927722208
Eden Chen <chened11@oocl.com>
2024-12-02 14:11:08 +0800

    feat: implement get todoList

---
+++ src/main/java/com/todo/entity/TodoItem.java
@@ -0,0 +1,39 @@
+package com.todo.entity;
+
+
+public class TodoItem {
+
+    private Integer id;
+    private String text;
+    private Boolean done;
+
+    public TodoItem(Integer id, String text, Boolean done) {
+        this.id = id;
+        this.text = text;
+        this.done = done;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Boolean getDone() {
+        return done;
+    }
+
+    public void setDone(Boolean done) {
+        this.done = done;
+    }
+}
